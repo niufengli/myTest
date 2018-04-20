ae9c485bbd1c9458eb4f68c743a87723566607d0
niufengli <niufengli@01zhuanche.com>
2018-04-20 12:14:54 +0800

    init

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Description:
+ *
+ * @author niufengli（niufengli@01zhuanche.com）
+ * @date 2018-04-20 12:12
+ * @copyright (c)2018 niufengli. Beijing. All Rights Reserved.
+ */
+
+
+public class Test {
+}
